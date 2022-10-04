530c5f217b59acd32898791f4ab3615673892d15
flandre <flantan233@gmail.com>
2022-10-04 18:05:10 +0800

    update

--- index.lua
+++ index.lua
@@ -1,4 +1,9 @@
--- 定义宠物技能状态
+-- 定义按键常量
+local CHANGE_EXTEND_SKILL_BTN = {'lctrl', 'v'} -- 定义切换附加技能快捷键
+local BACK_SKILL_KEY_TABLE = {'numperiod'} -- 定义返回主要技能快捷键
+local CANCEL_SUMMON_PET = {'y'} -- 定义取消宠物技能快捷键
+
+-- 定义宠物技能状态 
 function PetGroupStatus(currentSkillIndex, currentPetIndex)
     local o = {};
     o.currentSkillIndex = currentSkillIndex or 1;
@@ -6,94 +11,77 @@ function PetGroupStatus(currentSkillIndex, currentPetIndex)
     return o
 end
 
-function PetGroupCfgs(skillKeyTable, backSkillKeyTable, isChangeExtend, changeExtendKeyTable, petKeyTable, waitSleep)
+-- 定义宠物技能按键
+function PetGroupCfg(skillKeyTable, petKeyTable, isChangeExtend, waitSleep)
     local o = {}
     o.skillKeyTable = skillKeyTable
-    o.backSkillKeyTable = backSkillKeyTable
-    o.isChangeExtend = isChangeExtend or false
-    o.changeExtendKeyTable = changeExtendKeyTable
     o.petKeyTable = petKeyTable
+    o.isChangeExtend = isChangeExtend or false
     o.waitSleep = waitSleep or 500
+    return o
 end
 
+-- 组合键按键
+function PressKeyCfg(keyTable, sleep)
+    s = sleep or 10
+    if (type(keyTable) == 'table') then
+        for i, v in ipairs(keyTable) do
+            PressKey(keyTable)
+        end
+        sleep(s)
+        for i, v in ipairs(keyTable) do
+            ReleaseKey(keyTable)
+        end
+    else 
+        PressKey(keyTable)
+        sleep(s)
+        ReleaseKey(keyTable)
+    end
+end
 
-function DragonCallNext(currentIds,skills,isChange2,isColse)
-    OutputLogMessage("skillslength = %s  ",table.getn(skills));
-    OutputLogMessage("currentSkillIndex = %s  ",currentIds.currentSkillIndex);
-
-    --手动关闭宠物模式时，判断当前是否没有关闭宠物 ，如果没有则执行关闭的流程
-    if (currentIds.closeStatus == false) then
-            PressKey(skill.backSkill);
-            ReleaseKey(skill.backSkill);
-            --取消宠物后标记关闭状态
-            currentIds.closeStatus = true
-    else
-    
-    skill = skills[currentIds.currentSkillIndex];
-    pets = skill.petKeys;
-    pet = pets[currentIds.currentPetIndex];
-    OutputLogMessage("pet = %s \n",(currentIds.currentPetIndex));
-    --召唤前先取消宠物
-    PressKey(skill.backSkill);
-    ReleaseKey(skill.backSkill);
-    Sleep(100)
-    if(isChange2 == true) then
-            PressKey("lalt");
-            PressKey("v");
-            ReleaseKey("lalt");
-            ReleaseKey("v");
+-- 循环召唤宠物
+function SummonPetLoop(petGroupStatus, petGroupCfgs)
+    OutputLogMessage("===== start summon pet =====\n")
+    OutputLogMessage("petGroupCfgs length = %s\n", #petGroupCfgs)
+    OutputLogMessage("current skill index = %s\n", petGroupStatus.currentSkillIndex)
+    OutputLogMessage("current pet index = %s\n", petGroupStatus.currentPetIndex)
+    -- 开始循环召唤宠物
+    target = petGroupCfgs[petGroupStatus.currentSkillIndex]
+    pets = target.petKeyTable
+    pet = pets[PetGroupStatus.currentPetIndex]
+    -- 召唤前先取消宠物
+    PressKeyCfg(CANCEL_SUMMON_PET)
+    sleep(100)
+    -- 如果需要切换到附加技能栏，先切换到附加技能栏
+    if (target.isChangeExtend) then
+        PressKeyCfg(CHANGE_EXTEND_SKILL_BTN)
     end
-    --切换到技能栏
-    if(skill.skill == "4" or skill.skill == "3" or skill.skill == "2") then
-            PressKey("lctrl");
-            PressKey(skill.skill);
-            ReleaseKey("lctrl");
-            ReleaseKey(skill.skill);
-    else
-            PressKey(skill.skill);
-            ReleaseKey(skill.skill);
+    -- 切换到宠物技能栏
+    PressKeyCfg(target.skillKeyTable)
+    PressKeyCfg(pet)
+    -- 切换回默认技能栏
+    PressKeyCfg(BACK_SKILL_KEY_TABLE)
+    -- 如果切换到附加技能栏，则切换回来
+    if (target.isChangeExtend) then
+        PressKeyCfg(CHANGE_EXTEND_SKILL_BTN)
     end
-    --召唤宠物
-    PressKey(pet);
-    ReleaseKey(pet);
-    --Sleep(100)
-    --切换回默认技能栏
-  PressKey("lctrl");
-    PressKey("1");
-    ReleaseKey("lctrl");
-    ReleaseKey("1");
-
-    if(isChange2 == true) then
-            PressKey("lalt");
-            PressKey("v");
-            ReleaseKey("lalt");
-            ReleaseKey("v");
-    end
-    --此处设置延迟
-    Sleep(skill.waitSleep)
-    --Sleep(550)
-    --取消宠物
-    if(isColse == true) then
-            PressKey(skill.backSkill);
-            ReleaseKey(skill.backSkill);
-    else
-            --在手动关闭宠物的模式下 需要标记关闭状态为false
-            currentIds.closeStatus = false
-    end
-    --Sleep(300)
+    -- 宠物延时
+    sleep(target.waitSleep)
+    -- 取消召唤宠物
+    PressKeyCfg(CANCEL_SUMMON_PET
     --更新技能栏以及宠物键位
-    if(currentIds.currentPetIndex < table.getn(pets)) then
-            currentIds.currentPetIndex = currentIds.currentPetIndex + 1;
+    if(petGroupStatus.currentPetIndex < #pets) then
+        petGroupStatus.currentPetIndex = petGroupStatus.currentPetIndex + 1
     else
-            currentIds.currentSkillIndex = currentIds.currentSkillIndex + 1;
-            currentIds.currentPetIndex = 1;
+        petGroupStatus.currentSkillIndex = petGroupStatus.currentSkillIndex + 1
+        petGroupStatus.currentPetIndex = 1
     end
-    if(currentIds.currentSkillIndex > table.getn(skills)) then
-            currentIds.currentSkillIndex = 1;
-            currentIds.currentPetIndex = 1;
+    if(petGroupStatus.currentSkillIndex > #skills) then
+        petGroupStatus.currentSkillIndex = 1;
+        petGroupStatus.currentPetIndex = 1;
     end
-    end  
-    return currentIds;
+    return petGroupStatus
+    OutputLogMessage("===== end summon pet =====\n")
 end
 
 -- G键事件函数
