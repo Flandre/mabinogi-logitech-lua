ce742c92ee5c23aa29cb64fc888ed78a8dac5f9c
Flandre <flantan233@gmail.com>
2022-11-02 17:05:27 +0800

    update lua script

--- index.lua
+++ index.lua
@@ -1,9 +1,7 @@
--- 定义按键常量
-local CHANGE_EXTEND_SKILL_BTN = {'lctrl', 'v'} -- 定义切换附加技能快捷键
-local BACK_SKILL_KEY_TABLE = {'numperiod'} -- 定义返回主要技能快捷键
-local CANCEL_SUMMON_PET = {'y'} -- 定义取消宠物技能快捷键
+local CHANGE_EXTEND_SKILL_BTN = {'lctrl', 'v'} 
+local BACK_SKILL_KEY_TABLE = {'numperiod'}
+local CANCEL_SUMMON_PET = {'y'}
 
--- 定义宠物技能状态 
 function PetGroupStatus(currentSkillIndex, currentPetIndex)
     local o = {};
     o.currentSkillIndex = currentSkillIndex or 1;
@@ -11,7 +9,6 @@ function PetGroupStatus(currentSkillIndex, currentPetIndex)
     return o
 end
 
--- 定义宠物技能按键
 function PetGroupCfg(skillKeyTable, petKeyTable, isChangeExtend, waitSleep)
     local o = {}
     o.skillKeyTable = skillKeyTable
@@ -21,7 +18,6 @@ function PetGroupCfg(skillKeyTable, petKeyTable, isChangeExtend, waitSleep)
     return o
 end
 
--- 组合键按键
 function PressKeyCfg(keyTable, sleep)
     s = sleep or 10
     if (type(keyTable) == 'table') then
@@ -39,37 +35,36 @@ function PressKeyCfg(keyTable, sleep)
     end
 end
 
--- 循环召唤宠物
 function SummonPetLoop(petGroupStatus, petGroupCfgs)
     OutputLogMessage("===== start summon pet =====\n")
     OutputLogMessage("petGroupCfgs length = %s\n", #petGroupCfgs)
     OutputLogMessage("current skill index = %s\n", petGroupStatus.currentSkillIndex)
     OutputLogMessage("current pet index = %s\n", petGroupStatus.currentPetIndex)
-    -- 开始循环召唤宠物
+    
     target = petGroupCfgs[petGroupStatus.currentSkillIndex]
     pets = target.petKeyTable
     pet = pets[PetGroupStatus.currentPetIndex]
-    -- 召唤前先取消宠物
+    
     PressKeyCfg(CANCEL_SUMMON_PET)
     sleep(100)
-    -- 如果需要切换到附加技能栏，先切换到附加技能栏
+    
     if (target.isChangeExtend) then
         PressKeyCfg(CHANGE_EXTEND_SKILL_BTN)
     end
-    -- 切换到宠物技能栏
+    
     PressKeyCfg(target.skillKeyTable)
     PressKeyCfg(pet)
-    -- 切换回默认技能栏
+    
     PressKeyCfg(BACK_SKILL_KEY_TABLE)
-    -- 如果切换到附加技能栏，则切换回来
+    
     if (target.isChangeExtend) then
         PressKeyCfg(CHANGE_EXTEND_SKILL_BTN)
     end
-    -- 宠物延时
+    
     sleep(target.waitSleep)
-    -- 取消召唤宠物
-    PressKeyCfg(CANCEL_SUMMON_PET
-    --更新技能栏以及宠物键位
+    
+    PressKeyCfg(CANCEL_SUMMON_PET)
+    
     if(petGroupStatus.currentPetIndex < #pets) then
         petGroupStatus.currentPetIndex = petGroupStatus.currentPetIndex + 1
     else
@@ -84,7 +79,11 @@ function SummonPetLoop(petGroupStatus, petGroupCfgs)
     OutputLogMessage("===== end summon pet =====\n")
 end
 
--- G键事件函数
+-- register key group
+G5Status = PetGroupStatus(1, 1)
+G5Config = PetGroupCfg("num5", {"5", "6", "7", "8", "9", "0", "minus", "equal"}, false, 550)
+
+
 function OnEvent(event, arg)    
     OutputLogMessage("event = %s, arg = %s\n", event, arg);
     -- G1
@@ -106,6 +105,14 @@ function OnEvent(event, arg)
     -- G5
     if (event == "MOUSE_BUTTON_RELEASED" and arg == 5) then
         OutputLogMessage("in MOUSE_BUTTON_RELEASED 5 \n");
+        G5Status = SummonPetLoop(G5Status, G5Config)
+        Sleep(50)
+        G5Status = SummonPetLoop(G5Status, G5Config)
+        Sleep(50)
+        G5Status = SummonPetLoop(G5Status, G5Config)
+        Sleep(50)
+        G5Status = SummonPetLoop(G5Status, G5Config)
+        Sleep(50)
     end
     -- G6
     if (event == "MOUSE_BUTTON_RELEASED" and arg == 6) then
